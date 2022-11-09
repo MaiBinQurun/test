4f34a59cbbca772f0121ceedebd8d1584a96c710
pko <kjlk>
2022-11-09 12:17:07 +0300

    Initial Commit

---
+++ TestGit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestGit
+//
+//  Created by mai abdullah qurun on 15/04/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by mai abdullah qurun on 15/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
